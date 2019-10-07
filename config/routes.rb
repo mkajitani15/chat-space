74fc454284930a46aed335bb9d5d6492811a7a1b
mkajitani15 <m.kajitani.15@gmail.com>
2019-10-07 20:18:41 +0900

    add flashmessage
    フラッシュメッセージの表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages#index'
-  resources :users, only: [:edit, :update]
+  root 'messages#index'
+  resources :users, only: [:index, :edit, :update]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
